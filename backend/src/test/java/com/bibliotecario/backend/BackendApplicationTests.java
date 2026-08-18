7453b5de618e5d98b0788aef88b39cca2ec9a0c8
ana-maia-ribeiro <biamaiaribeiro@gmail.com>
2026-08-18 10:05:07 -0300

    Created Project

---
+++ backend/src/test/java/com/bibliotecario/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotecario.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
